2966df3262e154368a2df40acc443618fe325182
novahealth <it@novahealth.co.nz>
2019-05-29 16:35:28 +1200

    version 0.0.7
    fixed the code

---
+++ test.py
@@ -0,0 +1,13 @@
+import validate_nzbn
+
+t = validate_nzbn.validate_nzbn("999sadfs99asd91ss2399asdfasdf99")
+
+x = validate_nzbn.clean_nzbn("9222373ksj38474skdm3829aa10")
+
+print(t)
+print(x)
+
+# if t:
+#     print(t)
+# else:
+#     print("False")
--- validate_nzbn/__init__.py
+++ validate_nzbn/__init__.py
@@ -2,9 +2,19 @@
 #
 # REMEMBER TO CHANGE VERSION NUMBER IN "setup.py" WHEN MODIFYING CODE
 
+import re
+
+
+def clean_nzbn(nzbn):
+    nzbn_clean = re.sub("[^0-9]", "", nzbn)
+    return nzbn_clean
+
 
 def validate_nzbn(nzbn):
-    if len(nzbn) == 13:
-        return True
+    nzbn_to_validate = clean_nzbn(nzbn)
+
+    if len(nzbn_to_validate) == 13:
+        if nzbn_to_validate[0] == "9":
+            return nzbn_to_validate
     else:
-        return False
+        return nzbn
